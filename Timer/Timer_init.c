35b5d4e587c46450f9a6e82c4244fd4ca13159b3
noura <1701595@eng.asu.edu.eg>
2021-06-01 04:30:44 +0200

    Timer

---
+++ Timer/Timer_init.c
@@ -0,0 +1,13 @@
+#include "Timer_init.h"
+
+void init_timer (void)
+{
+	// disable the timer during setup
+	NVIC_ST_CTRL_R=0;
+	//reset the reload with the max value
+	NVIC_ST_RELOAD_R=0x00FFFFFF;
+	//clear the current reg
+	NVIC_ST_CURRENT_R=0;
+	// enable the clock of the processor 
+	NVIC_ST_CTRL_R=0x05;
+}
---
+++ Timer/Timer_init.h
@@ -0,0 +1,4 @@
+#include "stdint.h"
+#include "C:/Keil/TExaSware/tm4c123gh6pm.h"
+
+void init_timer (void);
