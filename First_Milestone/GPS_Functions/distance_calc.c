0647cb62c8ca8df2bb3e358c8741b93310aed573
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-06-02 21:44:48 +0200

    Added nmea_to_map function
    NMEA gives numbers that need to be converted into Longitude and Latitude in order to calculate the distance as it is on a map, this function converts NMEA format to Long/Lat

--- First_Milestone/GPS_Functions/distance_calc.c
+++ First_Milestone/GPS_Functions/distance_calc.c
@@ -1,13 +1,39 @@
 #include "functions.h"
 
-double rad(double degree){
+float rad(float degree){
 	//Calculates radian
 	return degree*PI/180;
+}
+float nmea_to_map(float nmea) {
+	//NMEA Format: dddmm.mmmm
+	//Longitude/Latitude = ddd + mm.mmmm/60
+	int ddd = nmea;
+	float mm;
+	if (nmea < 100) {
+		ddd = 0;
+		mm = nmea;
+	}
+	else {
+		//Calculating ddd
+		int th, h, t, u, h_th;  // Thousands,hundreds,tens,units 
+		u = ddd % 10;
+		t = (ddd / 10) % 10;
+		h = (ddd / 100) % 10;
+		th = ddd / 1000;
+		h_th = ddd - u - (t * 10) - (h * 100) - (th * 1000);
+		ddd = (h_th * 100) + (th * 10) + h; //First 3 or 2 digits
+		//Calculating mm.mmmm
+		mm = nmea - (h * 100) - (th * 1000) - (h_th * 10000);
+	}
+	return ddd + (mm / 60);
 }
 int distance_calc(float lat1, float lat2, float lng1, float lng2){
 	//Calculates the distance between two points given their  
 	//longitutes and latitudes according to Haversine Formula
-	
+	lat1 = nmea_to_map(lat1);
+	lat2 = nmea_to_map(lat2);
+	lng1 = nmea_to_map(lng1);
+	lng2 = nmea_to_map(lng2);
 	float dLat = rad(lat2 - lat1); 
 	float dLng = rad(lng2 - lng1);
 	
