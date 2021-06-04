5849cfd27478736e7a53885259651c4859a9e04f
Mahinour Amr <mahinouramr1998@gmail.com>
2021-06-04 21:02:38 +0200

    systick wait correction

--- First_Milestone/Timer/Timer.c
+++ First_Milestone/Timer/Timer.c
@@ -2,7 +2,7 @@
 void Systick_Wait_1ms ()
 {
 	NVIC_ST_CTRL_R = 0;
-	NVIC_ST_RELOAD_R = 16000-1;
+	NVIC_ST_RELOAD_R = 80000-1;
 	NVIC_ST_CURRENT_R=0;
 	NVIC_ST_CTRL_R = 0x05;
 	while((NVIC_ST_CTRL_R & 0x00010000)==0){}
