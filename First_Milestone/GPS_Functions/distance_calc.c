4c47b4c261a5788fa5ec2656357835b67d8b4f7f
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-06-04 12:57:01 +0200

    fixed errors

--- First_Milestone/GPS_Functions/distance_calc.c
+++ First_Milestone/GPS_Functions/distance_calc.c
@@ -7,8 +7,9 @@ float rad(float degree){
 float nmea_to_map(float nmea) {
 	//NMEA Format: dddmm.mmmm
 	//Longitude/Latitude = ddd + mm.mmmm/60
-	int ddd = nmea;
+	int ddd;
 	float mm;
+	ddd = nmea;
 	if (nmea < 100) {
 		ddd = 0;
 		mm = nmea;
@@ -30,16 +31,18 @@ float nmea_to_map(float nmea) {
 int distance_calc(float lat1, float lat2, float lng1, float lng2){
 	//Calculates the distance between two points given their  
 	//longitutes and latitudes according to Haversine Formula
+	float dLat, dLng, a, c;
+	int d;
 	lat1 = nmea_to_map(lat1);
 	lat2 = nmea_to_map(lat2);
 	lng1 = nmea_to_map(lng1);
 	lng2 = nmea_to_map(lng2);
-	float dLat = rad(lat2 - lat1); 
-	float dLng = rad(lng2 - lng1);
+	dLat = rad(lat2 - lat1); 
+	dLng = rad(lng2 - lng1);
 	
-	float a = sin(dLat / 2) * sin(dLat / 2) + cos(rad(lat1)) * cos(rad(lat2)) * sin(dLng / 2) * sin(dLng / 2);
-	float c = 2 * atan2(sqrt(a), sqrt(1 - a));
-	int d = c * R;
+	a = sin(dLat / 2) * sin(dLat / 2) + cos(rad(lat1)) * cos(rad(lat2)) * sin(dLng / 2) * sin(dLng / 2);
+	c = 2 * atan2(sqrt(a), sqrt(1 - a));
+	d = c * R;
 	
 	return d;
 }
