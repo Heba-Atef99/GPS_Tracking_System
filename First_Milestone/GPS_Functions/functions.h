76c9e005a8c3685459fc27fa88211cdc1910a9ee
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-06-04 16:31:19 +0200

    added nmea_to_map function seperately

--- First_Milestone/GPS_Functions/functions.h
+++ First_Milestone/GPS_Functions/functions.h
@@ -3,6 +3,8 @@
 #include "tm4c123gh6pm.h"
 
 #define PI 3.14159265358979323846
-#define R 6378137
+#define R 6371
+
 
 int distance_calc (float lat1, float lat2, float lng1, float lng2);
+float nmea_to_map(float nmea);
