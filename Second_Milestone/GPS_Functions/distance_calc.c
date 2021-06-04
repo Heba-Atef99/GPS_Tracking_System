4167bfee5b7533740984e20c974784d493aa4bd0
nermeen_atif <1701571@eng.asu.edu.eg>
2021-06-04 21:15:59 +0200

    added function folder

---
+++ Second_Milestone/GPS_Functions/distance_calc.c
@@ -0,0 +1,41 @@
+#include "functions.h"
+
+float rad(float degree){
+	//Calculates radian
+	return degree*PI/180;
+}
+int distance_calc(float lat1, float lat2, float lng1, float lng2){
+	//Calculates the distance between two points given their  
+	//longitutes and latitudes according to Haversine Formula
+	float dLat, dLng, a, c;
+	int d;
+	dLat = rad(lat2 - lat1); 
+	dLng = rad(lng2 - lng1);
+	
+	a = sin(dLat / 2) * sin(dLat / 2) + cos(rad(lat1)) * cos(rad(lat2)) * sin(dLng / 2) * sin(dLng / 2);
+	c = 2 * atan2(sqrt(a), sqrt(1 - a));
+	d = c * R;
+	
+	return d;
+}
+int Convert_fun(char str [],char N [],char S []){
+	//String str = String. valueOf(text);
+	char dest[100];
+	char dest2[100];
+	//char N[100];
+	//char S[100];
+	//char n [] ="$GPGLL,3003.91254,N,03116.81201,E,163727.00,A,A*6B";
+	size_t size;
+	char *ret;
+  const char ch = ',';
+	//size = sizeof(str);
+  ret = memchr(str , ch, strlen(str ));
+  strcpy(dest, ret+1);
+	//strncpy(dest, dest, 18);
+	ret = memchr(dest , ch, strlen(dest ));
+	strncpy(N, dest, 10);
+  ret = memchr(dest , ch, strlen(dest ));
+  strcpy(dest, ret+3);
+  strncpy(S, dest, 11);	
+	return 0;
+}
---
+++ Second_Milestone/GPS_Functions/functions.h
@@ -0,0 +1,11 @@
+#include "stdint.h"
+#include "math.h"
+#include "tm4c123gh6pm.h"
+
+#define PI 3.14159265358979323846
+#define R 6378137
+
+
+int distance_calc (float lat1, float lat2, float lng1, float lng2);
+float nmea_to_map(float nmea);
+int Convert_fun(char text [],char N [],char S []);
