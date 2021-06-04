f6ec793fff605c0ff586204538107a53b44af237
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-06-04 16:41:04 +0200

    moved nmea_to_map function seperately

--- First_Milestone/GPS_Functions/distance_calc.c
+++ First_Milestone/GPS_Functions/distance_calc.c
@@ -4,30 +4,6 @@ float rad(float degree){
 	//Calculates radian
 	return degree*PI/180;
 }
-float nmea_to_map(float nmea) {
-	//NMEA Format: dddmm.mmmm
-	//Longitude/Latitude = ddd + mm.mmmm/60
-	int ddd;
-	float mm;
-	ddd = nmea;
-	if (nmea < 100) {
-		ddd = 0;
-		mm = nmea;
-	}
-	else {
-		//Calculating ddd
-		int th, h, t, u, h_th;  // Thousands,hundreds,tens,units 
-		u = ddd % 10;
-		t = (ddd / 10) % 10;
-		h = (ddd / 100) % 10;
-		th = ddd / 1000;
-		h_th = ddd - u - (t * 10) - (h * 100) - (th * 1000);
-		ddd = (h_th * 100) + (th * 10) + h; //First 3 or 2 digits
-		//Calculating mm.mmmm
-		mm = nmea - (h * 100) - (th * 1000) - (h_th * 10000);
-	}
-	return ddd + (mm / 60);
-}
 int distance_calc(float lat1, float lat2, float lng1, float lng2){
 	//Calculates the distance between two points given their  
 	//longitutes and latitudes according to Haversine Formula
