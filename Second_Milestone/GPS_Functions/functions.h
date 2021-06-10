1f1aa7113cb848e34234087d12ffa4c4faa6f74f
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-06-10 02:34:51 +0200

    changed distance_calc return type to float

--- Second_Milestone/GPS_Functions/functions.h
+++ Second_Milestone/GPS_Functions/functions.h
@@ -8,7 +8,7 @@
 #define PI 3.14159265358979323846
 #define R 6378137
 
-int distance_calc (float lat1, float lat2, float lng1, float lng2);
+float distance_calc (float lat1, float lat2, float lng1, float lng2);
 float nmea_to_map(float nmea);
 uint8_t Convert_fun(char str [],char N[],char S[]);
 void gps_read(char [100]);
